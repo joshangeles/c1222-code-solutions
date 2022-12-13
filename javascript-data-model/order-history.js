4b61a7fa221fa117440a98e473e3c680aab355b7
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-13 18:14:26 +0000

    First Attempt at javascript-data-model exercise. -JA

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,67 @@
+var orderHistory = [
+  {
+    orderPlaced: '8/4/2020',
+    delivered: '8/8/2020',
+    ordertotal: 34.00,
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    itemInformation:
+  [{ title: 'JavaScript for impatient programmers', seller: 'Rauschmayer, Dr. Axel', price: 31.55, image: 'bookcover' }]
+  },
+  {
+    orderPlaced: '7/19/2020',
+    delivered: '7/20/2020',
+    ordertotal: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    itemInformation: [
+      {
+        title: 'The Timeless Way of Building',
+        seller: 'Alexander, Christopher',
+        price: 44.53,
+        image: 'bookcover',
+        lastReturnDate: '8/19/2020'
+      }]
+  },
+  {
+    orderPlaced: '7/4/2020',
+    delivered: '7/7/2020',
+    ordertotal: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    itemInformation: [
+      {
+        title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        seller: 'noneListed',
+        price: 17.22,
+        image: 'controller adapter thumbnail',
+        lastReturnDate: '8/5/2020'
+      }
+    ]
+  },
+  {
+    orderPlaced: '7/3/2020',
+    delivered: '7/5/2020',
+    ordertotal: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    itemInformation: [
+      {
+        title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        seller: 'noneListed',
+        price: 94.95,
+        image: 'Gamecube Controller Box',
+        lastReturnDate: '8/4/2020'
+      },
+      {
+        title: 'The Art of Sql',
+        seller: 'Faroult, Stephanie',
+        price: 33.99,
+        image: 'bookcover',
+        lastReturnDate: '8/4/2020'
+      }
+    ]
+  }
+];
+
+console.log('Completed Order History', orderHistory);
