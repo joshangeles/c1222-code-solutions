519041dd121f48a9029db1db0f58cb73ae0fa97a
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-19 22:55:38 +0000

    Completed array-manipulation-2. Updated pseudo-code to match approach on 'chunk' function and added logic on how to handle leftover values -JA

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -3,8 +3,8 @@
    Make empty storage sub-array
    Go through each index of input array and push() each found value to sub-array if sub-array contains less values than specified size
    When specified size is hit, push filled sub-array to storage array and empty sub array but:
-   If the current index plus the specified size is greater than the input array length then push leftover values into storage array...
-   without grouping into the sub-array */
+   If the current index plus the specified size minus 1 (to account for value at current index) is greater than the input array length then push leftover values into sub-array...
+   and push() sub-array to storage array even if specified chunk size is not met */
 function chunk(array, size) {
   var chunk = [];
   var chunkedValues = [];
@@ -15,6 +15,9 @@ function chunk(array, size) {
     if (chunkedValues.length === size) {
       chunk.push(chunkedValues);
       chunkedValues = [];
+    }
+    if ((i + size - 1) > array.length) {
+      chunk.push(chunkedValues);
     }
   }
   return chunk;
