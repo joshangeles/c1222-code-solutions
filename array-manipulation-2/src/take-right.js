677973f33e1988eaf537fa68f75e8aa8ac127f37
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-19 18:19:22 +0000

    Working on 'take-right' function, failing last 2 test cases at moment of committing. -JA

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,19 @@
 /* exported takeRight */
+/* make storage array for extracted values
+   Go through each index starting from whatever index is the specified count away from the end and push found values to storage array
+   BUT:
+   if the specified count is larger than the array length just extract the entire array and push each value into storage array
+   return storage array
+   if the input */
+function takeRight(array, count) {
+  var takenRightArray = [];
+  for (var i = (array.length - count); i < array.length; i++) {
+    if (count > array.length) {
+      for (i = 0; i < array.length; i++) {
+        takenRightArray.push(array[i]);
+      }
+    }
+    takenRightArray.push(array[i]);
+  }
+  return takenRightArray;
+}
