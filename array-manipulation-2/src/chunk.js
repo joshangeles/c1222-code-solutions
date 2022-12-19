65ddcb35935603cda7d10231ae85a5c433d3ec1a
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-19 22:02:28 +0000

    'chunk' function passing 3/5 test cases, working on logic to ignore chunking leftover values

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,21 @@
 /* exported chunk */
+/* Make empty storage array
+   Make empty storage sub-array
+   Go through each index of input array and push() each found value to sub-array if sub-array contains less values than specified size
+   When specified size is hit, push filled sub-array to storage array and empty sub array but:
+   If the current index plus the specified size is greater than the input array length then push leftover values into storage array...
+   without grouping into the sub-array */
+function chunk(array, size) {
+  var chunk = [];
+  var chunkedValues = [];
+  for (var i = 0; i < array.length; i++) {
+    if (chunkedValues.length < size) {
+      chunkedValues.push(array[i]);
+    }
+    if (chunkedValues.length === size) {
+      chunk.push(chunkedValues);
+      chunkedValues = [];
+    }
+  }
+  return chunk;
+}
--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,6 +1,12 @@
 /* exported dropRight */
 /* Make empty storage array
-   Go through each index of array and  */
+   Go through each index of array and push found value to storage array but:
+   Stop going through each index when the current index is the specified count away from the end of the array
+   Return the now filled storage array */
 function dropRight(array, count) {
-
+  var dropRight = [];
+  for (var i = 0; i < (array.length - count); i++) {
+    dropRight.push(array[i]);
+  }
+  return dropRight;
 }
