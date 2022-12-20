d99525bda1574643977b4ca672e70048b383ec69
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-20 04:06:29 +0000

    Completed form-values.js -JA

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -3,8 +3,9 @@ function submitHandler(event) {
   submittedForm.name = $contactForm.elements.name.value;
   submittedForm.email = $contactForm.elements.email.value;
   submittedForm.message = $contactForm.elements.message.value;
+  console.log(submittedForm);
+  $contactForm.reset();
 }
 var $contactForm = document.querySelector('#contact-form');
 var submittedForm = {};
 $contactForm.addEventListener('submit', submitHandler);
-console.log(submittedForm);
