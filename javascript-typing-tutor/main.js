00756b729625114b2e6b9d75b8d7e0d452caabd4
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-22 19:32:42 +0000

    Implemented removal of underline from green characters, now need to add underline being added to next character

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -9,11 +9,17 @@ var $characters = document.querySelectorAll('span');
 var currentCharacter = 0;
 function characterHandler(event) {
   if (event.key === $characters[currentCharacter].textContent) {
-    console.log('key u pressed same as current character congratz lol');
-    currentCharacter++;
+    if (currentCharacter < $characters.length) {
+      console.log('key u pressed same as current character congratz lol');
+      $characters[currentCharacter].className += ' green';
+      if ($characters[currentCharacter].className.includes('underline')) {
+        $characters[currentCharacter].className = 'green';
+      }
+      currentCharacter++;
+      $characters[currentCharacter].className = 'underline';
+    }
   } else {
     // dont increment da currentcharacter and make it gross n red n underlined lol
-    console.log('spacebar test: ', event.key);
   }
   return currentCharacter;
 }
