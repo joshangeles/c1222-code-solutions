203b2b98fc8c92632909a750975cb55767491dce
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-23 05:10:39 +0000

    Finished 'defaults' function and all functions pass test cases. -JA

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,21 +1,21 @@
 /* exported defaults */
-/* Go through each key found in source and:
-   Check if that key matches any key found in target
+/*
+   Go through each key found in source and:
+   Check if that key value matches any key value found in target
    If it does NOT match any key then...
-   Add that key to target object and assign its value as the value associated with that key in source object */
+   Add that key to target object and assign its value as the value associated with that key in source object BUT
+   If the values found are different but the keys exist in both object do not assign the source value to the target object
+*/
 function defaults(target, source) {
-  // var sourceKeys = Object.keys(source);
   var targetKeys = Object.keys(target);
   if (targetKeys.length === 0) {
     for (var sourceKey in source) {
       target[sourceKey] = source[sourceKey];
     }
   }
-  for (sourceKey in source) {
-    for (var targetKey in target) {
-      if (targetKey !== sourceKey) {
-        target[sourceKey] = source[sourceKey];
-      }
+  for (var sourceProperty in source) {
+    if (source[sourceProperty] !== target[sourceProperty] && !targetKeys.includes(sourceProperty)) {
+      target[sourceProperty] = source[sourceProperty];
     }
   }
 }
