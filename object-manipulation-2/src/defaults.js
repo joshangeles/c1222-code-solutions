759f511c06dba715f63d27d423530e0a5ec1d7ea
Joshua Angeles <mintoesverm@yahoo.com>
2023-01-24 22:07:53 +0000

    Changed logic to not turn object into an array. -JA

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,20 +1,11 @@
 /* exported defaults */
-/*
-   Go through each key found in source and:
-   Check if that key value matches any key value found in target
-   If it does NOT match any key then...
-   Add that key to target object and assign its value as the value associated with that key in source object BUT
-   If the values found are different but the keys exist in both object do not assign the source value to the target object
-*/
+/* Go through each key found in source and:
+   Check if that key has a property with a value in the target object
+   If it does NOT then...
+   Add that value to the target property and assign the value of the associated property in the source object to it */
 function defaults(target, source) {
-  var targetKeys = Object.keys(target);
-  if (targetKeys.length === 0) {
-    for (var sourceKey in source) {
-      target[sourceKey] = source[sourceKey];
-    }
-  }
   for (var sourceProperty in source) {
-    if (source[sourceProperty] !== target[sourceProperty] && !targetKeys.includes(sourceProperty)) {
+    if (target[sourceProperty] === undefined) {
       target[sourceProperty] = source[sourceProperty];
     }
   }
