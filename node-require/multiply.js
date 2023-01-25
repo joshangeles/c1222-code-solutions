2b04b6f653caa273e83e2d5cf67169971c5e2b1f
Joshua Angeles <mintoesverm@yahoo.com>
2023-01-25 00:15:59 +0000

    Completed node-require exercise. -JA

---
+++ node-require/multiply.js
@@ -0,0 +1,5 @@
+function multiply(multiplicand, multiplier) {
+  console.log('Result:', Number(multiplicand) * Number(multiplier));
+}
+
+module.exports.multiply = multiply;
---
+++ node-require/subtract.js
@@ -0,0 +1,5 @@
+function subtract(minuend, subtrahend) {
+  console.log('Result:', Number(minuend) - Number(subtrahend));
+}
+
+module.exports.subtract = subtract;
