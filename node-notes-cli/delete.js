8db8fca933a020c7428b26011c23e6df430a20c4
Joshua Angeles <mintoesverm@yahoo.com>
2023-01-26 19:32:07 +0000

    Added guard clause to 'delete' module.

--- node-notes-cli/delete.js
+++ node-notes-cli/delete.js
@@ -2,6 +2,10 @@ const data = require('./data.json');
 const fs = require('fs');
 
 function del(id) {
+  if (data.notes[id] === undefined) {
+    console.log('\x1b[36m%s\x1b[0m', `Entry #${id} not found! Does this note exist?`);
+    return;
+  }
   delete data.notes[id];
   let updatedData = data;
   updatedData = JSON.stringify(updatedData, null, 2);
