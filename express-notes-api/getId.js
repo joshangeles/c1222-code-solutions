3b1cce63a1484b43994e772656cb81bde866a06c
Joshua Angeles <mintoesverm@yahoo.com>
2023-01-29 04:01:59 +0000

    Implemented basic functionality of 'getId.js'

--- express-notes-api/getId.js
+++ express-notes-api/getId.js
@@ -1,5 +1,21 @@
 function getId(app) {
-
+  const fs = require('fs');
+  let model = null;
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) console.log('Error!');
+    model = JSON.parse(data);
+  });
+  app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    switch (true) {
+      case ((id < 1 || isNaN(id) || id % 1 !== 0)):
+        res.status(400).json({ error: 'id must be a positive integer' });
+        break;
+      case model.notes[id] !== undefined:
+        res.status(200).json(model.notes[id]);
+        break;
+    }
+  });
 }
 
 module.exports = getId;
--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const getList = require('./getList');
+const getId = require('./getId');
 
 const express = require('express');
 const port = 3000;
@@ -8,7 +9,7 @@ const expressJSON = express.json();
 app.use(expressJSON);
 
 getList(app);
-
+getId(app);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
