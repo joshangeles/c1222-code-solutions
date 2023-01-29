7d3ca5fd18f78843ea72ac2f58c2a5f743c1c046
Joshua Angeles <mintoesverm@yahoo.com>
2023-01-29 02:27:51 +0000

    Changed getList module to output data matching requested format

---
+++ express-notes-api/getId.js
@@ -0,0 +1,5 @@
+function getId(app) {
+
+}
+
+module.exports = getId;
--- express-notes-api/getList.js
+++ express-notes-api/getList.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-console */
 function getList(app) {
   const fs = require('fs');
-  let notes = null;
+  let model = null;
   fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) console.log('Error!');
-    notes = JSON.parse(data);
+    model = JSON.parse(data);
   });
   app.get('/api/notes', (req, res) => {
+    const notes = [];
+    for (const key in model.notes) {
+      notes.push(model.notes[key]);
+    }
     res.status(200).json(notes);
   });
 }
