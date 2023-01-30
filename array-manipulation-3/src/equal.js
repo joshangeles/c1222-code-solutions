77b818db06029108edbc2d9ae8ca5d89a9b6df58
Joshua Angeles <mintoesverm@yahoo.com>
2023-01-30 21:33:51 +0000

    Completed 'equal.js' and started functionality of 'flatten.js'

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,1 +1,32 @@
 /* exported equal */
+/*
+  Declare and initialize a boolean to true
+  Go through each value in whichever array is longer but:
+  If the arrays are the same length use the length of the first array then:
+  Compare the values found at that index in both arrays to each other
+  If they are not strictly equals then assign false to the boolean
+  Return the boolean
+*/
+
+function equal(first, second) {
+  let isEquals = true;
+  let highestLength = null;
+  switch (true) {
+    case first.length > second.length:
+      highestLength = first.length;
+      break;
+    case second.length > first.length:
+      highestLength = second.length;
+      break;
+    default:
+      highestLength = first.length;
+  }
+  for (let i = 0; i < highestLength; i++) {
+    const firstValue = first[i];
+    const secondValue = second[i];
+    if (firstValue !== secondValue) {
+      isEquals = false;
+    }
+  }
+  return isEquals;
+}
--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,19 @@
 /* exported flatten */
+/*
+  Declare and initialize storage array
+  Go through each index of array and:
+  Check if the value at each index is an array and:
+  If TRUE- loop through the values in the subarray and push to storage array
+  If FALSE- Push value to storage array
+*/
+
+function flatten(array) {
+  const flattenedArr = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (Array.isArray(element)) {
+      console.log(element);
+    }
+  }
+  return flattenedArr;
+}
