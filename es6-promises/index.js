990c973a668bd21444e8204220999ca30653e034
Joshua Angeles <mintoesverm@yahoo.com>
2023-02-01 21:34:52 +0000

    Changed then() call to use both then() and catch()

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -2,8 +2,10 @@ const takeAChance = require('./take-a-chance');
 
 const chance = takeAChance('Josh');
 
-chance.then((result) => {
-  console.log(result);
-}, (err) => {
-  console.log(err.message);
-});
+chance
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
