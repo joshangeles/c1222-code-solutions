32f001c164bff5063039aeb23e7890d52a75b75f
Joshua Angeles <mintoesverm@yahoo.com>
2023-02-03 04:20:32 +0000

    Changed route modules to contain callback functions to be passed into 'index.js' for the sake of readability. -JA

--- sgt-back-end/get.js
+++ sgt-back-end/get.js
@@ -2,55 +2,53 @@ const db = require('./db');
 const { json, jsonError } = require('./status');
 const { validate } = require('./validate');
 
-function get(app) {
-  app.get('/api/grades', (req, res) => {
-    const sql = `
-    SELECT *
-      FROM "grades"
-      ORDER BY "gradeId" ASC
-    ;
-  `;
-    db.query(sql)
-      .then((result) => {
-        const grade = result.rows;
+const getList = (req, res) => {
+  const sql = `
+  SELECT *
+    FROM "grades"
+    ORDER BY "gradeId" ASC
+  ;
+`;
+  db.query(sql)
+    .then((result) => {
+      const grade = result.rows;
+      json(res, 200, grade);
+    })
+    .catch((err) => {
+      console.error(err);
+      jsonError(res, 500, 'An unexpected error occurred.');
+    });
+};
+
+const getId = (req, res) => {
+
+  const sql = `
+  SELECT *
+    FROM "grades"
+   WHERE "gradeId" = $1
+   ;
+`;
+
+  const gradeId = Number(req.params.gradeId);
+  const params = [gradeId];
+
+  if (!(validate(gradeId, 'not required', 'not required', 'not required', res))) {
+    return;
+  }
+
+  db.query(sql, params)
+    .then((result) => {
+      const grade = result.rows[0];
+      if (!grade) {
+        jsonError(res, 400, `Grade #${gradeId} not found!`);
+      } else {
         json(res, 200, grade);
-      })
-      .catch((err) => {
-        console.error(err);
-        jsonError(res, 500, 'An unexpected error occurred.');
-      });
-  });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      jsonError(res, 500, 'An unexpected error occurred.');
+    });
+};
 
-  app.get('/api/grades/:gradeId', (req, res) => {
-
-    const sql = `
-    SELECT *
-      FROM "grades"
-     WHERE "gradeId" = $1
-     ;
-  `;
-
-    const gradeId = Number(req.params.gradeId);
-    const params = [gradeId];
-
-    if (!(validate(gradeId, 'not required', 'not required', 'not required', res))) {
-      return;
-    }
-
-    db.query(sql, params)
-      .then((result) => {
-        const grade = result.rows[0];
-        if (!grade) {
-          jsonError(res, 400, `Grade #${gradeId} not found!`);
-        } else {
-          json(res, 200, grade);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        jsonError(res, 500, 'An unexpected error occurred.');
-      });
-  });
-}
-
-module.exports = get;
+module.exports = { getList, getId };
--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const port = 3000;
 const app = express();
-const get = require('./get');
+const { getList, getId } = require('./get');
 const post = require('./post');
-const put = require('./put');
-const remove = require('./remove');
+const updateGrade = require('./put');
+const deleteGrade = require('./remove');
 
 app.use(express.json());
 
-get(app);
-post(app);
-put(app);
-remove(app);
+app.get('/api/grades', getList);
+app.get('/api/grades/:gradeId', getId);
+app.post('api/grades/', post);
+app.put('api/grades/:gradeId', updateGrade);
+app.delete('/api/grades/:gradeId', deleteGrade);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
