7605a4df3e9f10da4fa5ca70fb655910e6887d65
Joshua Angeles <mintoesverm@yahoo.com>
2023-02-09 18:43:35 +0000

    Completed react-accordion-component challenge

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -2,25 +2,23 @@
 import React, { useState } from 'react';
 
 export default function Accordion({ inputData }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <div>
-      {inputData.map((entry) => Bellow(entry))}
+      {inputData.map((entry) => Bellow(entry, activeIndex, setActiveIndex))}
     </div>
   );
 }
 
-function Bellow({ title, id, details }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleClick = (e) => {
-    setIsOpen(!isOpen);
-  };
+function Bellow({ title, id, details }, activeIndex, setActiveIndex) {
 
   return (
-    <div key={id} className='bellow' onClick={handleClick}>
+    <div key={id} className='bellow' onClick={() => {
+      (activeIndex !== id) ? setActiveIndex(id) : setActiveIndex(0);
+    }}>
       <h3>{title}</h3>
-      <div hidden={!isOpen}>{details}</div>
+      <div hidden={activeIndex !== id}>{details}</div>
     </div>
   );
 }
-// extract single bar as a component then build accordion by mapping each object to generate individual bars in the accordion then return it
