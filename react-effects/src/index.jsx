bac1d9b0ba70e619ca037033da8867e07c45a119
Joshua Angeles <mintoesverm@yahoo.com>
2023-02-10 19:32:11 +0000

    Completed react-effects exercise v3.1415926

---
+++ react-effects/src/index.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars -- Remove me */
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import readItems from './read.js';
+
+function List() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState([]);
+
+  // Your code here:
+  //  - Read the items using `readItems` and update state so the list displays
+
+  const storedPromise = readItems();
+
+  useEffect(() => {
+    storedPromise.then((items) => {
+      setItems(items);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <ul>
+      {items.map((item) => <li key={item.id}>{item.id}: {item.name}</li>)}
+    </ul>
+  );
+}
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(<List />);
