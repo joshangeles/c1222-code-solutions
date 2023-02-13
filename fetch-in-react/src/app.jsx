b35ff5a98342eeb57eeef244f0217d56cd7fc858
Joshua Angeles <mintoesverm@yahoo.com>
2023-02-13 21:59:42 +0000

    Completed fetch-in-react exercise. -JA

---
+++ fetch-in-react/src/app.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars -- Remove this line. */
+import React, { useState, useEffect } from 'react';
+import UserList from './user-list';
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+
+  /* your code here (hint: useEffect) */
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return isLoading
+    ? <p>Loading...</p>
+    : <UserList users={users} />;
+}
