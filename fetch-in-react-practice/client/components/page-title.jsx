d6317264c81c0579f7399b152ec005ba0b999efc
Joshua Angeles <mintoesverm@yahoo.com>
2023-02-14 09:54:16 -0800

    Revert "Created fetch-in-react-practice basic functionality"

--- fetch-in-react-practice/client/components/page-title.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function PageTitle({ text }) {
-  return (
-    <h1 className="mb-4">{ text }</h1>
-  );
-}
--- fetch-in-react-practice/database/data.sql
+++
@@ -1,4 +0,0 @@
-insert into "todos" ("task", "isCompleted")
-values ('Learn to code', false),
-       ('Build projects', false),
-       ('Get a job', false);
